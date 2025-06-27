b8b7cd5bf116d4a9ff18e202e6236bcb376b2444
Mikołaj Siatkowski <msiatkowski@keemail.me>
2025-06-27 15:29:00 +0200

    feat: structure for two way traffic (#2)

--- src/sim_consts.rs
+++ src/sim_consts.rs
@@ -1,6 +1,6 @@
 pub const FIXED_DT: f64 = 1.0 / 2.0;
 pub const MAX_FRAME_TIME: f64 = 0.25;
-pub const SIM_SPEED_MULTIPLIER: f64 = 25.0;
+pub const SIM_SPEED_MULTIPLIER: f64 = 50.0;
 
 pub const LIGHT_CYCLE_SECONDS: f64 = 10.0;
 
